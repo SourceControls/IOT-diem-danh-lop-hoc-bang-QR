ea290e973c1cfec49c57ab4f0f6a4a5b7e04eec5
TuanHung <hungbuituan1@gmail.com>
2022-12-22 15:40:49 +0700

    update

--- Back-End/server/controllers/sensorController.js
+++ Back-End/server/controllers/sensorController.js
@@ -10,17 +10,37 @@ class SensorControllers {
     myobj.IDLSV = parseInt(myobj.IDLSV)
     console.log("SensorData: ", myobj);
     //BLUETOTH CÓ THỂ BỎ QUA TRƯỜNG lat và lng, trường IP có thể chứa giá trị của bluetooth
-    // https://localhost:8080/sensor?IDBUOIHOC=2&IDLSV=1&IP=127.0.0.1&lat=10.8489687&lng=106.7960183
+    //khoảng cách ở nhà 230/14-manthieejn
+    // https://localhost:8080/sensor?IDBUOIHOC=2&IDLSV=1&IP=127.0.0.1&lat=10.848936&lng=106.795772
 
+    //khoảng cách ở trường
+    // http://localhost:8080/sensor?IDBUOIHOC=2&IDLSV=2&IP=127.0.0.1&lat=10.848085&lng=106.786452
     try {
 
-      //cập nhật lại trạng thái điểm danh của sinh viên
 
+      let BUOIHOC = (await doRequest('http://localhost:8080/BUOIHOC/GetList', { IDBUOIHOC: myobj.IDBUOIHOC }))[0];
+
+      //check xem buổi học đó còn điểm danh được không
+      if (BUOIHOC.SUBMITTED || !BUOIHOC.STARTTIME) {
+        console.log(BUOIHOC.SUBMITTED, BUOIHOC.STARTTIME);
+        res.send("FAIL!! Ngoài giờ điểm danh 1"); return;
+      }
+      let currentHour = (new Date()).getHours();
+      let currentMinutes = (new Date()).getMinutes();
+      let [buoiHocHour, buoiHocMinutes] = BUOIHOC.STARTTIME.split(':');
+      let totalCurrentMinutes = parseInt(currentHour) * 60 + parseInt(currentMinutes);
+      let totalBuoiHocMinutes = parseInt(buoiHocHour) * 60 + parseInt(buoiHocMinutes);
+      //đã quá 15 phút
+      if ((totalCurrentMinutes - totalBuoiHocMinutes) >= 15) {
+        res.send("FAIL!! Ngoài giờ điểm danh 3"); return;
+      }
+      //cập nhật lại trạng thái điểm danh của sinh viên
       let updated = (await doRequest('http://localhost:8080/CT_DiemDanh/Update', { query: { IDBUOIHOC: myobj.IDBUOIHOC, IDLSV: myobj.IDLSV }, newValue: { DADIEMDANH: true } }))
-      // if (updated == 0) { res.send("FAIL!! Sinh viên đã điểm danh trước đó"); return; }
+
+      if (updated == 0) { res.send("FAIL!! Sinh viên đã điểm danh trước đó"); return; }
       //phát hiện gian lận và cập nhật vào ghi chú nếu có
-      let MAGV = (await doRequest('http://localhost:8080/BUOIHOC/GetList', { IDBUOIHOC: myobj.IDBUOIHOC }));
-      MAGV = MAGV[0].IDLGVSUBMITTED;
+
+      let MAGV = BUOIHOC.IDLGVSUBMITTED;
       let studentPosition = { lat: myobj.lat, lng: myobj.lng };
       let giangVienPosition = io.getGiangVienPosition(MAGV);
       var GHICHU = cheatDetector.dectect(myobj.IDBUOIHOC, myobj.IP, giangVienPosition, studentPosition)
